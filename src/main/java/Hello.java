8aa0c5e7368f345ab96b48a8f22917ba2b2f3b56
Hemachandar <ultimatehem@gmail.com>
2017-01-24 14:56:29 +0800

    Updated Hello world

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
   
-  public static String GREETING = "Hello world!";
+  public static String GREETING = "Hello world Changed!";
   
   public static void main(String []args) {
     
